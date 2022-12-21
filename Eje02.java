ca43b3d86de29bdb1503a4197fb04bf07eb72ea2
miguelfal <falconmiguel.2004@gmail.com>
2022-12-21 20:56:15 +0100

    Commit

---
+++ Eje02.java
@@ -0,0 +1,16 @@
+/* Ejercicio 2
+Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+pantalla de tal forma que el resultado del programa sea el mismo que en el
+ejercicio 1 del capítulo 1. 
+
+@author miguelfalcon
+*/
+
+public class Eje02 {
+    public static void main(String[] args) {
+
+        String nombre="Miguel Falcón";
+        System.out.print(nombre);
+
+    }
+}
